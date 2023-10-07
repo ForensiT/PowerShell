644f05333339fbfad6426ac4a5aa296bdcae27d8
Dylan <57426223+kDylanHayes@users.noreply.github.com>
2023-10-07 10:58:25 -0400

    Updated to only install needed Graph modules
    - Limit what is installed to speed up the process 
    - Use compare objects to check if modules are installed and only try installing what is missing
    - Updated capital casing

--- Save-AzureADUser.ps1
+++ Save-AzureADUser.ps1
@@ -1,18 +1,18 @@
-# Check that Microsoft.Graph is installed
-if (-Not (Get-Module -ListAvailable -Name Microsoft.Graph)) {
-
-    $install = Read-Host 'The Microsoft Graph PowerShell module is not installed. Do you want to install it now? (Y/n)'
-
-    if ($install -eq '' -Or $install -eq 'Y' -Or $install -eq 'Yes') {
+# Check that the needed Microsoft Graph Modules are installed and install only if needed
+$Modules = "Microsoft.Graph.Authentication", "Microsoft.Graph.Identity.DirectoryManagement", "Microsoft.Graph.Users"
+$ToInstall = Compare-Object (Get-Module -ListAvailable -Name $Modules) $Modules
+if ($ToInstall) {
+    $Install = Read-Host 'The Microsoft Graph PowerShell module is not installed. Do you want to install it now? (Y/n)'
+    if ($Install -eq '' -Or $Install -eq 'Y' -Or $Install -eq 'Yes') {
         If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
             Write-Warning "Administrator permissions are needed to install the Microsoft Graph PowerShell module.`nPlease re-run this script as an Administrator."
             Exit
         }
-        Write-Host "Installing"
-        Install-Module -Name Microsoft.Graph
+        Write-Host "Installing: $($ToInstall.InputObject)"
+        Install-Module $ToInstall.InputObject
     }
     else {
-        exit
+        Exit
     }
 }
 #Connect to Microsoft Graph and get all users
@@ -20,38 +20,38 @@ Connect-MgGraph -Scopes Directory.Read.All -NoWelcome
 $EntraUsers = Get-MgUser -All
 
 # Create the XML file
-$xmlsettings = New-Object System.Xml.XmlWriterSettings
-$xmlsettings.Indent = $true
-$xmlsettings.IndentChars = "    "
-$XmlWriter = [System.XML.XmlWriter]::Create("$((Get-Location).Path)\ForensiTAzureID.xml", $xmlsettings)
+$XMLSettings = New-Object System.Xml.XmlWriterSettings
+$XMLSettings.Indent = $true
+$XMLSettings.IndentChars = "    "
+$XMLWriter = [System.XML.XmlWriter]::Create("$((Get-Location).Path)\ForensiTAzureID.xml", $XMLSettings)
 
 # Write the XML Declaration and set the XSL
-$xmlWriter.WriteStartDocument()
-$xmlWriter.WriteProcessingInstruction("xml-stylesheet", "type='text/xsl' href='style.xsl'")
+$XMLWriter.WriteStartDocument()
+$XMLWriter.WriteProcessingInstruction("xml-stylesheet", "type='text/xsl' href='style.xsl'")
 
 # Start the Root Element 
-$xmlWriter.WriteStartElement("ForensiTAzureID")
+$XMLWriter.WriteStartElement("ForensiTAzureID")
 
 # Write the Entra ID domain details as attributes
 $Context = Get-MgContext
-$xmlWriter.WriteAttributeString("ObjectId", $Context.TenantId)
-$xmlWriter.WriteAttributeString("Name", (get-MgDomain).id);
+$XMLWriter.WriteAttributeString("ObjectId", $Context.TenantId)
+$XMLWriter.WriteAttributeString("Name", (Get-MgDomain).Id);
 $TenantName = (Invoke-RestMethod -UseBasicParsing -Uri ("https://login.microsoftonline.com/GetUserRealm.srf?login=$($Context.Account)")).FederationBrandName
-$xmlWriter.WriteAttributeString("DisplayName", $TenantName);
+$XMLWriter.WriteAttributeString("DisplayName", $TenantName);
 
 #Parse the data
 ForEach ($EntraUser in $EntraUsers) {
   
-    $xmlWriter.WriteStartElement("User")
-    $xmlWriter.WriteElementString("UserPrincipalName", $($EntraUser.UserPrincipalName))
-    $xmlWriter.WriteElementString("ObjectId", $($EntraUser.Id))
-    $xmlWriter.WriteElementString("DisplayName", $($EntraUser.DisplayName))
-    $xmlWriter.WriteEndElement()
+    $XMLWriter.WriteStartElement("User")
+    $XMLWriter.WriteElementString("UserPrincipalName", $($EntraUser.UserPrincipalName))
+    $XMLWriter.WriteElementString("ObjectId", $($EntraUser.Id))
+    $XMLWriter.WriteElementString("DisplayName", $($EntraUser.DisplayName))
+    $XMLWriter.WriteEndElement()
 }
 
-$xmlWriter.WriteEndElement()
+$XMLWriter.WriteEndElement()
 # Close the XML Document
-$xmlWriter.WriteEndDocument()
-$xmlWriter.Flush()
-$xmlWriter.Close()
+$XMLWriter.WriteEndDocument()
+$XMLWriter.Flush()
+$XMLWriter.Close()
 write-host "Entra ID user file created: $((Get-Location).Path)\ForensiTAzureID.xml"
